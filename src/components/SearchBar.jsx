852618cdf22a4ba69ef79b49c7c4022a19c5020e
TTRAN70 <tonytran124589@gmail.com>
2023-04-29 20:20:12 -0400

    CSS: styled the search bar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,9 +1,20 @@
+import "./SearchBar.css";
+import { HiOutlineLocationMarker } from "react-icons/hi";
+import { HiMagnifyingGlass } from "react-icons/hi2";
 const SearchBar = () => {
   return (
     <div className="search-bar">
       <form className="search" action="/weather">
-        <input required type="text" name="search" placeholder="Search" />
-        <button type="submit">Search</button>
+        <button className="searchInput" type="submit">
+          <HiMagnifyingGlass className="searchIcon" />
+        </button>
+        <input
+          className="weatherInput"
+          required
+          type="text"
+          name="search"
+          placeholder="Search"
+        />
       </form>
     </div>
   );
