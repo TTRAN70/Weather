eddc24b8ed1024dd2711b60aee582c7b41e3c6ab
TTRAN70 <tonytran124589@gmail.com>
2023-05-03 13:58:46 -0400

    Fixed routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
 import { useState } from "react";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  HashRouter,
+} from "react-router-dom";
 import SearchBar from "./components/SearchBar";
 import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/Weather-App/" element={<SearchBar />} />
-          <Route path="/Weather-App/weather" element={<Weather />} />
-        </Routes>
-      </Router>
+      <HashRouter>
+        <Router>
+          <Routes>
+            <Route path="/Weather-App/" element={<SearchBar />} />
+            <Route path="/Weather-App/weather" element={<Weather />} />
+          </Routes>
+        </Router>
+      </HashRouter>
     </div>
   );
 }
