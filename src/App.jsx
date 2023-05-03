901240f652402ca536ef7ed65f34094746ca6f16
TTRAN70 <tonytran124589@gmail.com>
2023-05-03 14:09:54 -0400

    Route bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,18 @@
 import { useState } from "react";
 import "./App.css";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  HashRouter,
-} from "react-router-dom";
+import { Router, Routes, Route } from "react-router-dom";
 import SearchBar from "./components/SearchBar";
 import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
-      <HashRouter>
-        <Router>
-          <Routes>
-            <Route path="/Weather-App/" element={<SearchBar />} />
-            <Route path="/Weather-App/weather" element={<Weather />} />
-          </Routes>
-        </Router>
-      </HashRouter>
+      <Router>
+        <Routes>
+          <Route path="/Weather-App/" element={<SearchBar />} />
+          <Route path="/Weather-App/weather" element={<Weather />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
